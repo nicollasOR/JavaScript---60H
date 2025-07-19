70449ef6676234ac664a31aa365b7523209f0a0d
FIC <nicollas.oliveira.rufino30@gmail.com>
2025-07-19 16:31:49 -0300

    primeiro JS

---
+++ NodeJS/Variaveis (Bool Int e etc)/variaveis de logica.js
@@ -0,0 +1,10 @@
+var a = 10;
+
+const b = 20;
+
+let c = 30;
+
+
+console.log(a);
+console.log(b);
+console.log(c);
---
+++ NodeJS/Variaveis (String e etc)/console.js
@@ -0,0 +1,21 @@
+
+console.log("Hello World");
+console.log("12345");
+console.log("#@*$&)!");
+
+
+console.log(typeof ("hello world"));
+console.log(typeof ("12345"));
+console.log(typeof ("#@*$&)!"));
+
+console.log('console.log("hello World")');
+
+console.log(true);
+console.log(false);
+
+console.log(typeof (false));
+console.log(typeof (true));
+
+
+
+
