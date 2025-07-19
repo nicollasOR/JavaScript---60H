0834e4155472d1887dedebdb2144ca687090da0c
FIC <nicollas.oliveira.rufino30@gmail.com>
2025-07-19 16:07:23 -0300

    ultimo CSS

---
+++ CSS/atividades/sim.java
@@ -0,0 +1,20 @@
+public class exemplo01{
+public static void main(string[]args){
+    Scanner ler=new canner(System.in);
+int x;
+System.out.println("digite sua idade ");
+x=ler.nextInt();
+if(x>18) 
+System.out.println("entrada liberada ");
+else if (x==18)
+System.out.println("entrada liberada");
+else
+System.out.println("entrada proibiida");
+System.out.println("fim do programa");
+
+
+
+
+}
+
+}
